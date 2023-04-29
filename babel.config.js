31da4a1657f01c785423d299cf975349add3f917
Michael Taciuk <taciuk73@gmail.com>
2023-04-29 13:50:58 -0400

    Added API_URL to babel allowlist

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,16 @@
 module.exports = function(api) {
   api.cache(false);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:react-native-dotenv'],
     plugins: [
       ["module:react-native-dotenv", {
         "envName": "APP_ENV",
         "moduleName": "@env",
         "path": ".env",
         "blocklist": null,
-        "allowlist": null,
+        "allowlist": [
+          "API_URL",
+        ],
         "safe": false,
         "allowUndefined": true,
         "verbose": false
